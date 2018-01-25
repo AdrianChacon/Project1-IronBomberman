ad228394a72d112111f189923d0f1ea84438383b
Adrián Chacón <adrianchaconmadueno@gmail.com>
2018-01-25 22:37:59 +0100

    Control de teclas, recolocado

--- js/main.js
+++ js/main.js
@@ -4,48 +4,56 @@ var tecla1 = [], tecla2 = [];
 
 $(document).keydown(function (e) {
     if (tecla1.length == 0) {
-        tecla1.push('quieto1');
         switch (e.which) {
             case 32: // p1.bomb
+                tecla1.push('quieto1');
                 bomberman1.dropTheBomb();
                 break;
             case 37: // p1.left
+                tecla1.push('quieto1');
                 bomberman1.moveX(-1);
                 bomberman1.collisionDirX = -1
                 break;
             case 38: // p1.up
+                tecla1.push('quieto1');
                 bomberman1.moveY(-1);
                 bomberman1.collisionDirY = -1
                 break;
             case 39: // p1.right
+                tecla1.push('quieto1');
                 bomberman1.moveX(1);
                 bomberman1.collisionDirX = 1
                 break;
             case 40: // p1.down
+                tecla1.push('quieto1');
                 bomberman1.moveY(1);
                 bomberman1.collisionDirY = 1
                 break;
         };
     }
     if (tecla2.length == 0) {
-        tecla2.push('quieto2')
         switch (e.which) {
             case 84: // p2.bomb
+                tecla2.push('quieto2')
                 bomberman2.dropTheBomb();
                 break;
             case 65: // p2.left
+                tecla2.push('quieto2')
                 bomberman2.moveX(-1);
                 bomberman2.collisionDirX = -1
                 break;
             case 87: // p2.up
+                tecla2.push('quieto2')
                 bomberman2.moveY(-1);
                 bomberman2.collisionDirY = -1
                 break;
             case 68: // p2.right
+                tecla2.push('quieto2')
                 bomberman2.moveX(1);
                 bomberman2.collisionDirX = 1
                 break;
             case 83: // p2.down
+                tecla2.push('quieto2')
                 bomberman2.moveY(1);
                 bomberman2.collisionDirY = 1
                 break;
